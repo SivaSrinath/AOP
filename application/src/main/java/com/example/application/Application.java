253840beefc48cfa27612dd682cf128ea77a5e98
srinath <sivasrinath19@gmail.com>
2018-12-10 22:58:09 -0500

    datasource example

--- application/src/main/java/com/example/application/Application.java
+++ application/src/main/java/com/example/application/Application.java
@@ -1,7 +1,12 @@
 package com.example.application;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 /**
  * 
  * @author Srinivas Nangana
@@ -9,8 +14,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		DataSourceTransactionManager dsTransactionManager = new DataSourceTransactionManager(dataSource);
+		return dsTransactionManager;
+	}
+	
+	
 }
--- application/src/main/java/com/example/application/service/ApplicationService.java
+++ application/src/main/java/com/example/application/service/ApplicationService.java
@@ -1,5 +1,8 @@
 package com.example.application.service;
 
+
+
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import com.example.application.bean.Student;
@@ -7,6 +10,7 @@ import com.example.application.bean.Student;
 @Service
 public class ApplicationService {
 
+	@Transactional
 	public Student getStudent() {
 
 		return new Student(1, "Srinivas");
